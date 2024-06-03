d40f1c687e36bab9f86043748dca6c575be29e7f
Ronan Talboom <ronant.talboom@ipknowledge.net>
2024-06-03 11:19:37 +0200

    add admin login

---
+++ admin/index.php
@@ -0,0 +1,10 @@
+<?php
+
+session_start();
+if ($_SESSION["admin_id"]) {
+    header("location:dashboard.php");
+} else {
+    header("location:login.php");
+}
+
+?>
--- includes/checklogin.php
+++ includes/checklogin.php
@@ -1,11 +1,20 @@
 <?php
 function check_login()
 {
-if(strlen($_SESSION['id'])==0)
-	{	
-		
-		$_SESSION["id"]="";
-		header("Location: logout.php");
-	}
+    if (strlen($_SESSION["id"]) == 0) {
+        $_SESSION["id"] = "";
+        header("Location: logout.php");
+    }
+} ?>
+
+<?php function check_admin_login()
+{
+    if (strlen($_SESSION["admin_id"]) == 0) {
+        if (strlen($_SESSION["id"]) == 0) {
+            header("Location:logout.php");
+        } else {
+            header("Location:dashboard.php");
+        }
+    }
 }
 ?>
