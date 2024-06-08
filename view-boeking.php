e26507e99019dffe2c3d097df9027149d2bf74df
Ronan Talboom <ronantalboom@hotmail.com>
2024-06-08 15:45:19 +0200

    updates to use new database

--- view-boeking.php
+++ view-boeking.php
@@ -3,94 +3,7 @@ session_start();
 include "includes/config.php";
 include "includes/checklogin.php";
 //code for add courses
-if (isset($_POST["submit"])) {
-    $klantID = $_POST["klantID"];
-    $boeking_datum = $_POST["boeking_datum"];
-    $checkin_datum = $_POST["checkin_datum"];
-    $checkuit_datum = $_POST["checkuit_datum"];
-    $status = $_POST["status"];
-    $boeking_datumd = date("Y-m-d H:i:s", strtotime($boeking_datum));
-    $checkin_datumd = date("Y-m-d H:i:s", strtotime($checkin_datum));
-    $checkuit_datumd = date("Y-m-d H:i:s", strtotime($checkuit_datum));
-    if (isset($_POST["plaats"]) && isset($_POST["tarieven"])) {
-        $plaats = $_POST["plaats"];
-        $query = "SELECT * FROM `plaats_boekingen` INNER JOIN `boekingen` 
-		ON plaats_boekingen.boeking_id = boekingen.boekingID WHERE plaats_id = ? 
-		AND ((checkin_datum < ? 
-		AND checkuit_datum > ?) 
-		OR (checkin_datum < ? 
-		AND checkuit_datum > ?) 
-		OR (checkin_datum > ? 
-		AND checkuit_datum < ?)
-		OR (checkin_datum <= ? 
-		AND checkuit_datum >= ?))";
-        $stmt = $conn->prepare($query);
-        $stmt->bind_param(
-            "issssssss",
-            $plaats,
-            $checkin_datumd,
-            $checkin_datumd,
-            $checkuit_datumd,
-            $checkuit_datumd,
-            $checkin_datumd,
-            $checkuit_datumd,
-            $checkin_datumd,
-            $checkuit_datumd
-        );
-        $stmt->execute();
-        $res = $stmt->get_result();
-        if ($res->num_rows > 0) {
-            // The "plaats" has already been booked by a different "boeking" on the selected date
-            echo "<script>alert('This plaats has already been booked on the selected date');</script>";
-        } else {
-            $query =
-                "INSERT INTO `boekingen`(`klantID`,`boeking_datum`, `checkin_datum`, `checkuit_datum`, `status`) VALUES (?,?,?,?,?)";
-            $stmt = $conn->prepare($query);
-            $rc = $stmt->bind_param(
-                "issss",
-                $klantID,
-                $boeking_datumd,
-                $checkin_datumd,
-                $checkuit_datumd,
-                $status
-            );
-            $stmt->execute();
-            $boekingID = $conn->insert_id;
 
-            // The "plaats" is available on the selected date
-            $query = "INSERT INTO `plaats_boekingen`(`plaats_id`, `boeking_id`) VALUES (?,?)";
-            $stmt = $conn->prepare($query);
-            $stmt->bind_param(
-                "ii",
-                $plaats,
-                $boekingID,
-            );
-            $stmt->execute();
-            foreach ($_POST["tarieven"] as $tarief) {
-                $query = "SELECT kosten FROM `tarieven` WHERE ID=?";
-                $stmt = $conn->prepare($query);
-                $rc = $stmt->bind_param(
-                    "i",
-                    $tarief
-                );
-                $stmt->execute();
-                $res = $stmt->get_result();
-                while ($row = $res->fetch_object()) {
-                    $query = "INSERT INTO `boeking_tarieven`(`boekingID`, `tariefID`, `kosten`) VALUES (?,?,?)";
-                    $stmt = $conn->prepare($query);
-                    $rc = $stmt->bind_param(
-                        "iid",
-                        $boekingID,
-                        $tarief,
-                        $row->kosten
-                    );
-                    $stmt->execute();
-                }
-            }
-            header("location:boekingen.php");
-        }
-    }
-}
 ?>
 <!doctype html>
 <html lang="en" class="no-js">
@@ -131,12 +44,12 @@ if (isset($_POST["submit"])) {
                                     <div class="panel-heading">
                                         <?php
                                         $boekingID = $_GET["id"];
-                                        $query = "SELECT plaats_boekingen.plaats_id, boekingen.boekingID, boekingen.klantID, boekingen.checkin_datum, boekingen.checkuit_datum, tarieven.kosten 
+                                        $query = "SELECT plaats_boekingen.plaats_id, boekingen.id, boekingen.klant_id, boekingen.checkin_datum, boekingen.checkuit_datum, tarieven.kosten 
                                         FROM plaats_boekingen 
-                                        INNER JOIN boekingen ON plaats_boekingen.boeking_id = boekingen.boekingID 
-                                        INNER JOIN boeking_tarieven ON boekingen.boekingID = boeking_tarieven.boekingID 
-                                        INNER JOIN tarieven ON boeking_tarieven.tariefID = tarieven.ID
-                                        WHERE boekingen.boekingID = ?";
+                                        INNER JOIN boekingen ON plaats_boekingen.boeking_id = boekingen.id 
+                                        INNER JOIN boeking_tarieven ON boekingen.id = boeking_tarieven.boeking_id 
+                                        INNER JOIN tarieven ON boeking_tarieven.tarief_id = tarieven.id
+                                        WHERE boekingen.id = ?";
                                         $stmt = $conn->prepare($query);
                                         $stmt->bind_param("i", $boekingID);
                                         $stmt->execute();
@@ -145,7 +58,7 @@ if (isset($_POST["submit"])) {
                                         if ($row) {
                                             // Access the data in $row here
                                         ?>
-                                            <h2>Reservering #<?php echo $row->boekingID; ?> </h2>
+                                            <h2>Reservering #<?php echo $row->id; ?> </h2>
                                     </div>
                                     <div class="panel-body">
                                         <dl class="row">
@@ -161,7 +74,7 @@ if (isset($_POST["submit"])) {
                                             <dd class="col-sm-9">
                                                 <dl class="row">
                                                     <?php
-                                                    $query = "SELECT * FROM plaatsen WHERE ID=?";
+                                                    $query = "SELECT * FROM plaatsen WHERE id=?";
                                                     $stmt = $conn->prepare($query);
                                                     $stmt->bind_param("i", $row->plaats_id);
                                                     $stmt->execute();
@@ -200,9 +113,9 @@ if (isset($_POST["submit"])) {
                                             <dd class="col-sm-9">
                                                 <dl class="row">
                                                     <?php
-                                                    $query = "SELECT * FROM klant WHERE klantID=?";
+                                                    $query = "SELECT * FROM klant WHERE id=?";
                                                     $stmt = $conn->prepare($query);
-                                                    $stmt->bind_param("i", $row->klantID);
+                                                    $stmt->bind_param("i", $row->id);
                                                     $stmt->execute();
                                                     $res = $stmt->get_result();
                                                     $klant = $res->fetch_object();
@@ -236,7 +149,7 @@ if (isset($_POST["submit"])) {
                                                 <dl class="row">
                                                     <?php
                                                     $query = "SELECT tarieven.beschrijving, tarieven.kosten FROM tarieven 
-                                                    INNER JOIN boeking_tarieven ON tarieven.ID = boeking_tarieven.tariefID 
+                                                    INNER JOIN boeking_tarieven ON tarieven.id = boeking_tarieven.tarief_id 
                                                     WHERE boeking_tarieven.boekingID = ?";
                                                     $stmt = $conn->prepare($query);
                                                     $stmt->bind_param("i", $boekingID);
