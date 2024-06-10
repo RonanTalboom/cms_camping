f276734f5bc3111363fdceb23cd0c4730370ea67
Ronan Talboom <ronantalboom@hotmail.com>
2024-06-10 10:38:44 +0200

    edit boeking werkt

--- delete-boeking.php
+++ delete-boeking.php
@@ -11,7 +11,7 @@ if (isset($_GET["id"])) {
     $rc = $stmt->bind_param("i", $id);
     $stmt->execute();
     
-    $query = "DELETE FROM boeking_tarieven WHERE id=?";
+    $query = "DELETE FROM boeking_tarieven WHERE boeking_id=?";
     $stmt = $conn->prepare($query);
     $rc = $stmt->bind_param("i", $id);
     $stmt->execute();
